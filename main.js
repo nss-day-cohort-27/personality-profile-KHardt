dab587499487b0935152fb0e59594e85bcfdea20
Klaus Hardt <klausheina@gmail.com>
2018-07-26 14:11:58 -0500

    OMG I didn't use the variable I created to pull from storage- just needed to plug in. Also took load and get storage out of functions

--- main.js
+++ main.js
@@ -17,24 +17,22 @@ const storageObject = {
 
 }
 
-//store data to local storage
-const saveDatabasePersonal = function (databaseObject, localStorageKeyPersonal) {
-    const stringifiedDatabase = JSON.stringify(databaseObject)
-   localStorage.setItem(localStorageKeyPersonal, stringifiedDatabase)
-}
+//store data to local storage- (took it out of a function)
 
-saveDatabasePersonal(storageObject, "Personality")
+const stringifiedDatabase = JSON.stringify(storageObject)
+localStorage.setItem("storageObject", stringifiedDatabase)
 
-//load data:
-const loadDatabasePersonal = function (localStorageKey) {
-    const databaseString = localStorage.getItem(localStorageKey)
-    return JSON.parse(databaseString)
-} 
 
-let data = loadDatabasePersonal("Personality")
+//load data: (took it out of a function)
 
-//write to DOM:
+const databaseString = localStorage.getItem("storageObject")
+let data = JSON.parse(databaseString)
 
+console.log(data); //testing only
+
+//write to DOM: 
+
+//failed experiment:
 /*const secCreate = document.createElement("section1");
 const t = document.createTextNode("hello")
 secCreate.appendChild(t)
@@ -45,13 +43,13 @@ let addStuff = document.querySelector(".wrapper")
 addStuff.innerHTML += 
 `<section> 
     <h2>General Profile</h2>
-    <p>${storageObject.statement1.statement} (agree)</p>
-    <p>${storageObject.statement2.statement} (nope-I'm all over the place)</p>
-    <p>${storageObject.statement3.statement} (generally agree)</p>
+    <p>${data.statement1.statement} (agree)</p>
+    <p>${data.statement2.statement} (nope-I'm all over the place)</p>
+    <p>${data.statement3.statement} (generally agree)</p>
     <h2>Wierdos like me: </h2>
-    <h3>${storageObject.peopleLikeMe}</h3>
+    <h3>${data.peopleLikeMe}</h3>
     <h2>How I prefer to be communicated to: </h2>
-    <h3>${storageObject.communication}</h3>
+    <h3>${data.communication}</h3>
     <h4>(kidding)</h4>
     </section>`
         
